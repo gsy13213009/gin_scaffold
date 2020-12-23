017e2e746a73e4d064a84c1313d760e04c132190
gsy <gsy@gsy.com>
2020-12-23 22:03:43 +0800

    添加接口文档实例，定义输入输出

--- controller/admin_login.go
+++ controller/admin_login.go
@@ -14,11 +14,22 @@ func AdminLoginRegister(group *gin.RouterGroup) {
 	group.POST("/login", adminLogin.AdminLogin)
 }
 
+// AdminLogin godoc
+// @Summary 管理员登录
+// @Description 管理员登录描述
+// @Tags 管理员接口
+// @ID /admin/login
+// @Accept  json
+// @Produce  json
+// @Param body body dto.AdminLoginInput true "body"
+// @Success 200 {object} middleware.Response{data=dto.AdminLoginOutput} "success"
+// @Router /admin/login [post]
 func (adminLogin *AdminLoginController) AdminLogin(c *gin.Context) {
 	params := &dto.AdminLoginInput{}
 	if err := params.BindValidParam(c); err != nil {
 		middleware.ResponseError(c, 1001, err)
 		return
 	}
-	middleware.ResponseSuccess(c, "")
+	out := &dto.AdminLoginOutput{Token: params.UserName}
+	middleware.ResponseSuccess(c, out)
 }
